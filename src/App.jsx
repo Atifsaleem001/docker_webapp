da10c22b287d241b885f036aaf17c2498f46d50b
Atif saleem <atifsaleem@Atifs-MacBook-Pro.local>
2026-06-18 21:32:15 +0530

    k8s

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>🚀 Docker Learning Project</h1>
+
+      <div className="card">
+        <h2>React + Docker</h2>
+        <p>
+          This application is running on React and will soon be containerized
+          using Docker.
+        </p>
+
+        <button>Docker is Awesome!</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
